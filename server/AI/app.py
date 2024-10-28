bee9b4c485a9ac723808a3770a7e3eca73d6f873
Utkarsh Sharma <utkarshdev2411@gmail.com>
2024-10-29 01:20:54 +0530

    Add .gitignore file to ignore unnecessary files and directories

--- server/AI/app.py
+++ server/AI/app.py
@@ -1,5 +1,6 @@
-# app.py
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from langchain_community.document_loaders import PyMuPDFLoader
+import pymupdf
 
 app = FastAPI()
 
@@ -8,6 +9,17 @@ def read_root():
     print("Server is running")
     return {"message": "Server is running"}
 
+@app.post("/pdf")
+def read_pdf():
+    pdf_path="sample.pdf"
+    try:
+        loader = PyMuPDFLoader(pdf_path)
+        doc = loader.load()
+        text = {}
+        for i in range(len(doc)):
+            text[i+1]=doc[i].page_content
+        return {"message": text}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-    
-# uvicorn app:app --reload
+# To run the server, use the command: uvicorn app:app --reload
