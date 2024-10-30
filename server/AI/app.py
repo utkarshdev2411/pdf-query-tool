5088532c43ce34af1893a73c61dff66215573bcb
Utkarsh Sharma <utkarshdev2411@gmail.com>
2024-10-30 13:19:19 +0530

    Add logging and unit test configuration for AI server
    - Introduced logging for better traceability in app.py
    - Added settings.json for unittest configuration in VSCode

--- server/AI/app.py
+++ server/AI/app.py
@@ -1,24 +1,46 @@
+import logging
 from fastapi import FastAPI, HTTPException
 from langchain_community.document_loaders import PyMuPDFLoader
 from database import init_db, insert_metadata
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+import google.generativeai as genai
+from dotenv import load_dotenv 
+import json
+
+import getpass
+import os
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     init_db()
+    logger.info("Database initialized")
 
 @app.get("/")
 def read_root():
-    print("Server is running")
+    logger.info("Server is running")
     return {"message": "Server is running"}
 
 @app.post("/pdf")
 def read_pdf():
     pdf_path = "sample.pdf"
     try:
+        logger.info("Starting PDF processing")
         loader = PyMuPDFLoader(pdf_path)
         doc = loader.load()
+        logger.info("PDF loaded successfully")
+        
         metadata = doc[0].metadata
         metadata_dict = {
             "source": pdf_path,
@@ -38,13 +60,24 @@ def read_pdf():
         }
         
         insert_metadata(metadata_dict)
+        logger.info("Metadata inserted into the database")
         
         text = {"metadata": metadata_dict}
+        text_db=""
         for i in range(len(doc)):
             text[i + 1] = doc[i].page_content
+            text_db+=doc[i].page_content
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+        chunks = text_splitter.split_text(text_db)
+        logger.info("Text split into chunks")
         
-        return {"message": text}
+        vector_store = FAISS.from_texts(chunks, embeddings)
+        vector_store.save_local("faiss_index")
+        logger.info("Vector store created and saved locally")
+        
+        return {"message": "Processing completed"}
     except Exception as e:
+        logger.error(f"Error during PDF processing: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
 # To run the server, use the command: uvicorn app:app --reload
