29bf0b90095015d37d18de8f119a444d1ebb6157
Utkarsh Sharma <utkarshdev2411@gmail.com>
2024-10-30 20:45:29 +0530

    Add chat functionality and integrate Google Generative AI for document processing

--- server/AI/app.py
+++ server/AI/app.py
@@ -9,6 +9,13 @@ import google.generativeai as genai
 from dotenv import load_dotenv 
 import json
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -87,6 +94,15 @@ def process_pdf(pdf_path: str):
     vector_store.save_local("faiss_index")
     logger.info("Vector store created and saved locally")
     
+    llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+    )
+    
     return {"message": "Processing completed"}
 
 @app.post("/pdf")
@@ -98,5 +114,49 @@ def read_pdf():
     except Exception as e:
         logger.error(f"Error during PDF processing: {e}")
         raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
+@app.post('/chat')
+def chat():
+    pdf_path = "sample.pdf"
+    loader = PyMuPDFLoader(pdf_path)
+    doc = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    splits = text_splitter.split_documents(doc)
+    vectorstore = InMemoryVectorStore.from_documents(
+    documents=splits, embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
+    
+    retriever = vectorstore.as_retriever()
+    
+    llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+    )
+    system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+    )
+    prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    results = rag_chain.invoke({"input": "give sumnmary of the document in 150 words"})
+    print(results["answer"])
+    
 
 # To run the server, use the command: uvicorn app:app --reload
